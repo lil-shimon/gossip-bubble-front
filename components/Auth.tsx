52733044463601990c7f382605ebb5362456f6fc
shimosawa <shimon101018@gmail.com>
2021-03-11 21:14:05 +0900

    implement auth func

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -1,32 +1,110 @@
+import {useRouter} from "next/router";
+import {ChangeEvent, FormEvent, useState} from "react";
+import Cookie from "universal-cookie";
+
+const cookie = new Cookie()
+
 export default function Auth() {
+    const router = useRouter()
+    const [username, setUsername] = useState<string>('')
+    const [password, setPassword] = useState<string>('')
+    const [isLogin, setIsLogin] = useState<boolean>(true)
+
+    //login func
+    const login = async () => {
+        try {
+            await fetch(
+                `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/auth/jwt/create`,
+                {
+                    method: "POST",
+                    body: JSON.stringify({username: username, password: password}),
+                    headers: {
+                        "Content-Type": 'application/json'
+                    }
+                }
+            )
+                .then((res) => {
+                    if (res.status === 400) {
+                        throw "authenticate failed"
+                    } else if (res.ok) {
+                        return res.json()
+                    }
+                })
+                .then((data) => {
+                    const options = {path: '/'}
+                    cookie.set('access_token', data.access, options)
+                })
+            await router.push('/main-page')
+        } catch (err) {
+            alert(err)
+        }
+    }
+
+    const authUser = async (e: FormEvent<HTMLFontElement>) => {
+        e.preventDefault()
+        if (isLogin) {
+            login()
+        } else {
+            try {
+                await fetch(
+                    `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/register/`,
+                    {
+                        method: "POST",
+                        body: JSON.stringify({username: username, password: password}),
+                        headers: {
+                            "Content-Type": 'application/json'
+                        }
+                    }
+                )
+                    .then((res) => {
+                        if (res.status === 400) {
+                            throw "authenticate failed"
+                        } else if (res.ok) {
+                            return res.json()
+                        }
+                    })
+            } catch (err) {
+                alert(err)
+            }
+            login()
+        }
+    }
+
     return (
         <div className="max-w-md w-full space-y-8">
             <div>
                 <img className="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
                      alt="Workflow"/>
                 <h2 className="mt-6 text-center text-3xl font-extrabold text-white">
-                    Sign in to your account
+                    {isLogin ? "login" : "sign up"}
                 </h2>
             </div>
-            <form className="mt-8 space-y-6" action="#" method="POST">
+            <form className="mt-8 space-y-6" onSubmit={authUser}>
                 <input type="hidden" name="remember" value="true"/>
                 <div className="rounded-md shadow-sm -space-y-px">
                     <div>
-                        <input id="email-address" name="email" type="email" autoComplete="email" required
+                        <input name="username" type="text" autoComplete="username" required
                                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                               placeholder="Email address"/>
+                               placeholder="User name"
+                               value={username}
+                               onChange={(e: ChangeEvent<HTMLInputElement>) => setUsername(e.target.value)}
+                        />
                     </div>
                     <div>
                         <input id="password" name="password" type="password" autoComplete="current-password"
                                required
                                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                               value={password}
+                               onChange={(e: ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
                                placeholder="Password"/>
                     </div>
                 </div>
 
                 <div className='flex items-center justify-center'>
                     <div className="text-sm">
-                    <span className="cursor-pointer font-medium text-white hover:text-indigo-500">
+                    <span className="cursor-pointer font-medium text-white hover:text-indigo-500"
+                          onClick={() => setIsLogin(!isLogin)}
+                    >
                         change mode?
                     </span>
                     </div>
@@ -44,7 +122,7 @@ export default function Auth() {
                                       clipRule="evenodd"/>
                             </svg>
                         </span>
-                        Sign in
+                        {isLogin ? "sign in with jwt" : "Create new user"}
                     </button>
                 </div>
             </form>
